d995709f450a1a0efc7cf860bab4552060df9f6c
王勇 <wy19945372694@163.com>
2023-08-10 15:04:25 +0800

    202308101504update

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 // var a = 3
 // ;(function () {})()[(1, 2, 3, 4)].forEach(e => {})
 
-var b = 4
-[1, 3].forEach(e => {
-    console.log(e)
-})
+// var b = 4
+// [1, 3].forEach(e => {
+//     console.log(e)
+// })
+
+// let arr = new Array(12)
+// console.log(arr.length);
+
